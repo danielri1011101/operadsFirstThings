1496e1fbea68db571ddcba3f6dc89c3292b9fd1f
Daniel R Barrero R <barreroroserodr@gmail.com>
2025-05-20 13:11:29 -0500

    added tree definitions

--- milewski/TicTacToe.hs
+++ milewski/TicTacToe.hs
@@ -28,3 +28,34 @@ ixV FinZ (x `VCons` _) = x
 ixV (FinS f_n)  (_ `VCons` xs) = ixV f_n xs
 
 -- Looks all like "Type-level list comprehensions", oder?
+
+type One = S Z
+type Two = S One 
+type Three = S Two
+
+-- Binding the elements of Fin Three:
+
+let fzero = FinZ :: Fin Three
+    fone = FinS FinZ :: Fin Three
+    ftwo = FinS (FinS FinZ) :: Fin Three
+
+-- Defining the move-trees for ticTacToe:
+
+data Trees n where
+  NilT :: Trees Z
+  Fan :: Trees n -> MoveTree n
+
+data MoveTree n where
+  Leaf :: MoveTree One
+  (:+) :: (Move, MoveTree k) -> Trees m -> Trees (k+m)
+
+-- Requires defining type-level/compile-time addition!
+-- Which a priori isn't commutative!
+
+class (Graded f) Operad (f :: Nat -> *) where
+  identity :: f (S Z)
+  compose :: f n -> Forest f m n -> f m
+
+-- Very reasonable, but one must notice that the total arity m is
+-- not yet partitioned as m = n1 + ... + nk, as in the traditional operad
+-- definition.
