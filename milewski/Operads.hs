5a57d60ca044e92b3c168ade6e32f1e871afb265
Daniel R Barrero R <barreroroserodr@gmail.com>
2025-06-09 13:39:16 -0500

    Constraint kinds and coercion done.

--- milewski/Operads.hs
+++ milewski/Operads.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ConstraintKinds #-}
 
 module Operads where
 
 import Numbers
 import Trees
+import Data.Kind
+import Unsafe.Coerce
 
 class Graded (f :: Nat -> *) where
   grade :: f n -> SNat n
@@ -14,7 +17,7 @@ class Graded (f :: Nat -> *) where
 -- explicitly stated, right?
 class (Graded f) => Operad (f :: Nat -> *) where
   ident :: f One
-  comp :: f n -> Forest f m n -> f m
+  compose:: f n -> Forest f m n -> f m
 
 instance Graded MoveTree where
   grade Leaf = SS SZ
@@ -24,16 +27,22 @@ instance Graded Trees where
   grade NilT = SZ
   grade ((_, t) :+ ts) = grade t `plus` grade ts
 
+-- ConstraintKinds language extension...
+-- Seems like making a type out of a constraint "by force"...
+data Dict :: Constraint -> * where
+  Dict :: a => Dict a
+
+plusZ :: forall n. Dict (n ~ (n + Z))
+plusZ = unsafeCoerce (Dict :: Dict (n ~ n))
+
 -- Define composition "recursively", from ground-up
 instance Operad MoveTree where
   ident = Leaf
-  comp Leaf (Cons t Nil) = t
-  comp (Fan ((mv, t) :+ ts)) frt =
-    let (mst1, mst2) = splitForest (grade t) frt
---       mst1 :: Forest MoveTree m1 (grade t)
---       mst2 :: Forest MoveTree m2 (grade (Fan ts))
---       _where_ frt :: Forest MoveTree (m1+m2) n
---       _where_ n = (grade t) + (grade (Fan ts))
-        tree = comp t mst1
-        (Fan trees) = comp (Fan ts) mst2
+  compose Leaf (Cons (t :: MoveTree m) Nil) =
+    case plusZ :: Dict (m ~ (m + Z)) of Dict -> t
+  compose (Fan ((mv, t) :+ ts)) frt =
+    let (mst1, mst2) = splitForest (grade t) (grade ts) frt
+        tree = compose t mst1
+        (Fan trees) = compose (Fan ts) mst2
     in Fan $ (mv,t) :+ trees
+
---
+++ milewski/ZOperads.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module ZOperads where
+
+import Numbers
+import Trees
+
+class Graded (f :: Nat -> *) where
+  grade :: f n -> SNat n
+
+-- Idty, equivariance, and associativity are assumed but not
+-- explicitly stated, right?
+class (Graded f) => Operad (f :: Nat -> *) where
+  ident :: f One
+  comp :: f n -> Forest f m n -> f m
+
+instance Graded MoveTree where
+  grade Leaf = SS SZ
+  grade (Fan ts) = grade ts
+
+instance Graded Trees where
+  grade NilT = SZ
+  grade ((_, t) :+ ts) = grade t `plus` grade ts
+
+-- Define composition "recursively", from ground-up
+instance Operad MoveTree where
+  ident = Leaf
+  comp Leaf (Cons t Nil) = t
+  comp (Fan ((mv, t) :+ ts)) frt =
+    let (mst1, mst2) = splitForest (grade t) frt
+--       mst1 :: Forest MoveTree m1 (grade t)
+--       mst2 :: Forest MoveTree m2 (grade (Fan ts))
+--       _where_ frt :: Forest MoveTree (m1+m2) n
+--       _where_ n = (grade t) + (grade (Fan ts))
+        tree = comp t mst1
+        (Fan trees) = comp (Fan ts) mst2
+    in Fan $ (mv,t) :+ trees
+
+splitForest :: SNat n1 -> SNat n2 -> Forest f m n -> (Forest f m1 n1, Forest f m2 n2)
