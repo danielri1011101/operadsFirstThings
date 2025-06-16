9b45f434fca86e0565cf71b00d3e0cbef9be9959
Daniel R Barrero R <barreroroserodr@gmail.com>
2025-06-16 11:46:42 -0500

    A _from-scratch_ monad to <<dually replicate>> the Comonad definitions.

--- milewski/Comonad.hs
+++ milewski/Comonad.hs
@@ -1,3 +1,11 @@
-class (Functor w) => Comonad w where
+-- extract is the counit.
+-- duplicate is the comultiplication.
+class Functor w => Comonad w where
   extract :: w a -> a
   duplicate :: w a -> w (w a)
+
+-- An "on the fly" monad for _dually replicating_ the
+-- Comonad definitions.
+class Functor m => Moonad m where
+  yoin :: m (m a) -> m a
+  riturn :: a -> m a
